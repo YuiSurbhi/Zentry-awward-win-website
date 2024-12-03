f620d221e0bd1c1f6649946f158013d5e5248d95
Surbhi Verma <vermasurbhi3665@gmail.com>
2024-12-03 17:48:11 +0530

    Updated file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
     <footer className='w-screen bg-violet-300 py-4 text-black'>
       <div className='container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row'>
         <p className='text-center text-sm md:text-left'>
-          &copy; Nova 2024, All right reserved
+          &copy; Surbhi Verma 2024, All right reserved
         </p>
 
         <div className='flex justify-center gap-4 md:justify-start'>
